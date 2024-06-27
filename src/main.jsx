5dada206965fd898c8b36cd31e60d5590e076803
Yaroslav <skylandtat@ukr.net>
2024-06-27 14:39:47 +0200

    Main: fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App";
 import "modern-normalize/modern-normalize.css";
-import "./index.css";
-import { store } from "../redux/store";
+import "./components/index.css";
+import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "../redux/store";
+import { persistor } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
