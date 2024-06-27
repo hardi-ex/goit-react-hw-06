e97f97f676187b87c45d082afd03642b52ab1425
Yaroslav <skylandtat@ukr.net>
2024-06-27 15:26:52 +0200

    Fixed and refactoring:  ContactList and SearchBox, delete hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,14 @@
 import ContactList from "./ContactList/ContactList";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactForm from "./ContactForm/ContactForm";
-import { useSelector } from "react-redux";
-import { selectContacts } from "../redux/contactsSlice";
-import { selectNameFilter } from "../redux/filtersSlice";
 
 const App = () => {
-  const contacts = useSelector(selectContacts) || [];
-  const filter = useSelector(selectNameFilter) || "";
-
-  const filteredList = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <>
       <h1>Phonebook</h1>
       <ContactForm />
-      <SearchBox searchList={filter} />
-      <ContactList contactsList={filteredList} />
+      <SearchBox />
+      <ContactList />
     </>
   );
 };
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,17 +1,20 @@
 import css from "./SearchBox.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeFilter } from "../../redux/filtersSlice";
 import { useId } from "react";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
-export const SearchBox = ({ searchList }) => {
+export const SearchBox = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(selectNameFilter);
+
   const idSearch = useId();
   return (
     <div className={css.div}>
       <label htmlFor={idSearch}>Find contacts by name</label>
       <input
         type="text"
-        value={searchList}
+        value={filter}
         id={idSearch}
         onChange={(evt) => dispatch(changeFilter(evt.target.value))}
       />
